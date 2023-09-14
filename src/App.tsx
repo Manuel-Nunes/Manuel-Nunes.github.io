c2c2739d84f9290e4bfed3f21d922539b44e1d0b
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-09-15 00:25:50 +0200

    Add base vite files;

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <p>lol</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Screens/CatShowCase.tsx
@@ -0,0 +1,9 @@
+function CatShowShowCase(){
+  return (
+    <>
+      <p>Hello Cat Show case</p>
+    </>
+  )
+}
+
+export default CatShowShowCase;
